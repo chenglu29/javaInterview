176385a441aa9a4e88b2e2c7a73677101896889a
chenglu29 <75212732+chenglu29@users.noreply.github.com>
2022-01-11 16:14:47 -0500

    Add files via upload

---
+++ numberofisland.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+/*
+Number of Islands
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+*/
+
+class numberofisland{
+	public static void main(String[] args){
+		int[][] M = {{1, 1, 0, 0, 0},
+                     {1, 1, 0, 0, 1},
+                     {1, 0, 0, 1, 1},
+                     {0, 0, 0, 0, 1},
+                     {1, 0, 1, 0, 1}};
+        int height=M.length;
+        int width=M[0].length;
+        int numberofislands=0;
+        for (int j=0;j<height;j++){
+        	for(int k=0;k<width;k++){
+        		if(M[j][k]==1){
+        			changezero(j,k,M);
+        			numberofislands+=1;
+        		}
+        	}
+        }
+        System.out.println(numberofislands);
+	}
+	public static void changezero(int i,int j, int [][]M){
+		if(i==M.length || j==M[0].length || i<0 || j<0)
+			return;
+		if(M[i][j]==0)
+			return;
+		M[i][j]=0;
+		changezero(i+1,j,M);//check downward
+		changezero(i, i+1,M); //check right
+		changezero(i-1, j,M); //check upward
+		changezero(i,j-1,M); //chec left
+
+	}
+}
