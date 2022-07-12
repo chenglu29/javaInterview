eb2dae66c06a99b096f9347a11b27c430ca1e922
Lu Cheng <75212732+chenglu29@users.noreply.github.com>
2022-07-11 21:04:13 -0400

    Add files via upload

---
+++ decode.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+ 
+class decode
+{
+    // Returns decoded string for 'str'
+    public static String decode(String s)
+    {
+       Stack<Integer> counts=new Stack();
+       Stack<String> result=new Stack();
+       String res="";
+       int index=0;
+       while(index<s.length()){
+         if(Character.isDigit(s.charAt(index))){
+            int count=0;
+            while(Character.isDigit(s.charAt(index))){
+                count=10*count+(s.charAt(index)-'0');
+                index+=1;
+            }
+            counts.push(count);
+
+        }else if(s.charAt(index)=='['){
+            result.push(res);
+            res="";
+            index+=1;
+
+        }else if(s.charAt(index)==']'){
+            StringBuilder temp=new StringBuilder(result.pop());
+            int count=counts.pop();
+            for(int i=0;i<count;i++){
+                temp.append(res);
+            }
+            res=temp.toString();
+            index+=1;
+
+
+        }else{
+            res+=s.charAt(index);
+            index+=1;
+        }
+       }
+       return res;
+
+
+    }
+ 
+    // Driver method
+    public static void main(String args[])
+    {
+        String s = "3[b2[ca]]";
+        System.out.println(decode(s));
+    }
+} 
+
